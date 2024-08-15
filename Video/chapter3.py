393d823153560fd65644a485eed14dbf8ef45c4b
Widiyanti004 <105841111621@student.unismuh.ac.id>
2024-08-15 22:27:21 +0800

    chapter3 Tkinter dan GUI

--- Video/chapter3.py
+++ Video/chapter3.py
@@ -23,36 +23,3 @@ reversed_video.write_videofile('reversed_result.mp4')
 # Kecepatan video menjadi 2 kali lebih cepat
 sped_up_video = short_video.fx(vfx.speedx, 2)
 sped_up_video.write_videofile('sped_up_result.mp4')
-
-import tkinter as tk
-
-# Membuat jendela utama
-root = tk.Tk()
-root.title("Aplikasi Multimedia")
-
-# Menjalankan perulangan acara Tkinter
-root.mainloop()
-
-from PIL import Image, ImageTk
-
-# Memuat image menggunakan Pillow
-image = Image.open('Teknologi.jpg')
-photo = ImageTk.PhotoImage(image)
-
-# Membuat label untuk menampilkan gambar
-label = tk.Label(root, image=photo)
-label.pack()
-
-from tkinter import filedialog
-from pydub import AudioSegment
-from pydub.playback import play
-
-def play_music():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        audio = AudioSegment.from_file(file_path)
-        play(audio)
-        
-        
-play_button = tk.Button(root, text="Play", command=play_music)
-play_button.pack()
